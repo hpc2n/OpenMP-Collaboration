fa9c5ce5a0b31951e5be1581d8b6c25674afe6de
Joachim Hein <joachim.hein@math.lu.se>
2023-09-06 17:24:44 +0200

    Update of runscipt in the c-code from K-kaise to COSMOS

--- Templates/Day_1/Running_OpenMP/SampleScript/C/job.sh
+++ Templates/Day_1/Running_OpenMP/SampleScript/C/job.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH -A SNIC2021-22-37
-#Asking for 3 min.
-#SBATCH -t 00:03:00
+#SBATCH -A lu2023-7-61
+
+#SBATCH -t 00:02:00             # Asking for 3 min.
 #SBATCH -c 2
 #SBATCH -J data_process         # name of job
 #SBATCH -o process_omp_%j.out   # output file
-#SBATCH -e process_omp_%j.err   # error messages
-#SBATCH --reservation=snic2021-22-37-dayX
+#SBATCH -e process_omp_%j.err           # error messages
+#SBATCH --reservation=lu2023-7-61_day1  # reservation change X for the day
 
 cat $0
 
 ml purge  > /dev/null 2>&1 
-#module load foss/2020b
-module load intel/2020a
+module load GCC/11.3.0
+#module load intel-compilers/2022.1.0
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
